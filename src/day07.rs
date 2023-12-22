b5dd32a554b30e5fa61313b00b09d7e504aec2b9
Mark90 <mark_moes@live.nl>
2023-12-22 14:48:08 +0100

    7.2

--- src/day07.rs
+++ src/day07.rs
@@ -19,17 +19,26 @@ struct Hand {
     bid: u32,
 }
 
-fn parse_line(raw_line: &str) -> Hand {
-    let line = raw_line.split_whitespace().collect::<Vec<&str>>();
-
+fn parse_hand_type(hand: &str, with_wildcard: bool) -> HandType {
     // Group chars and count occurrences, sort counts and map that to handtype
     // I thought this would be really elegant, the end result is a bit so-so
-    let mut groups: HashMap<char, u32> = HashMap::new();
-    for c in line[0].chars() {
-        groups.insert(c, groups.get(&c).unwrap_or(&0u32) + 1);
+    let mut card_count_map: HashMap<char, u32> = HashMap::new();
+    for c in hand.chars() {
+        card_count_map.insert(c, card_count_map.get(&c).unwrap_or(&0u32) + 1);
     }
-    let mut counts: Vec<&u32> = groups.values().collect::<Vec<&u32>>();
-    counts.sort_by(|a, b| b.cmp(&a));
+
+    let mut counts: Vec<u32>;
+    if with_wildcard && card_count_map.contains_key(&'J') && card_count_map.len() >= 2 {
+        // Change the joker to the card with the most occurences
+        let wildcard_count = card_count_map.remove(&'J').unwrap();
+        counts = card_count_map.values().map(|u| *u).collect::<Vec<u32>>();
+        counts.sort_by(|a, b| b.cmp(&a));
+        counts[0] = counts[0] + wildcard_count;
+    } else {
+        counts = card_count_map.values().map(|u| *u).collect::<Vec<u32>>();
+        counts.sort_by(|a, b| b.cmp(&a));
+    }
+
     let hand_type = match counts[..] {
         [5] => HandType::FiveOAK,
         [4, 1] => HandType::FourOAK,
@@ -40,16 +49,23 @@ fn parse_line(raw_line: &str) -> Hand {
         [1, 1, 1, 1, 1] => HandType::HighCard,
         _ => panic!("invalid card format or the parsing is broken"),
     };
+    hand_type
+}
 
-    // Convert str to hex (not using f and 0) then to int to get the hand strength
-    //  AKQJT987654321 -> edcba987654321
+fn parse_line(raw_line: &str, with_wildcard: bool, joker_char: char) -> Hand {
+    let line = raw_line.split_whitespace().collect::<Vec<&str>>();
+    let hand_type = parse_hand_type(&line[0], with_wildcard);
+
+    // Convert str to hex (not using f (and 0 for pt1)) then to int to get the hand strength
+    //  AKQJT987654321 -> edcba987654321 (without wildcard)
+    //  AKQJT987654321 -> edc0a987654321 (with wildcard)
     let hexadecimal: String = line[0]
         .chars()
         .map(|c| match c {
             'A' => 'e',
             'K' => 'd',
             'Q' => 'c',
-            'J' => 'b',
+            'J' => joker_char,
             'T' => 'a',
             _ => c,
         })
@@ -62,9 +78,13 @@ fn parse_line(raw_line: &str) -> Hand {
     }
 }
 
+fn parse_line_pt1(raw_line: &str) -> Hand {
+    parse_line(raw_line, false, 'b')
+}
+
 #[aoc(day7, part1)]
 fn part1(content: &str) -> u32 {
-    let mut hands: Vec<Hand> = content.lines().map(parse_line).collect();
+    let mut hands: Vec<Hand> = content.lines().map(parse_line_pt1).collect();
 
     // Sort hands by Type (worst to best) then Strength (weak to strong)
     hands.sort_by(|a, b| {
@@ -81,6 +101,29 @@ fn part1(content: &str) -> u32 {
     // 253313241
 }
 
+fn parse_line_pt2(raw_line: &str) -> Hand {
+    parse_line(raw_line, true, '0')
+}
+
+#[aoc(day7, part2)]
+fn part2(content: &str) -> u32 {
+    let mut hands: Vec<Hand> = content.lines().map(parse_line_pt2).collect();
+
+    // Sort hands by Type (worst to best) then Strength (weak to strong)
+    hands.sort_by(|a, b| {
+        b.hand_type
+            .cmp(&a.hand_type)
+            .then(a.strength.cmp(&b.strength))
+    });
+
+    hands
+        .iter()
+        .enumerate()
+        .map(|(idx, hand)| (idx + 1) as u32 * hand.bid)
+        .sum()
+    // 253362743
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -95,5 +138,10 @@ QQQJA 483";
     #[test]
     fn test_part_1() {
         assert_eq!(part1(&INPUT), 6440);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part2(&INPUT), 5905);
     }
 }
